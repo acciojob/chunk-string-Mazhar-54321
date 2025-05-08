d0e8818cb687e62661ebc994c9a216ec933e177d
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 08:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	let res=[];
+  let maxLength = Math.floor(str.length/size)*size;
+  let leftOverLength = str.length-maxLength;
+  for(let i=0;i<maxLength;i+=size){
+     res.push(str.substring(i,i+size));
+  }
+  if(leftOverLength>0){
+	res.push(str.substring(maxLength))
+  }
+  return res; 
 }
 
 // Do not change the code below
